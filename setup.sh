1b38f86779b9c8c77d60748b7668b80cddebcc89
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2020-10-11 11:29:44 +0200

    Add a script for setting up a build environment
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Create a folder where we will save everything
+mkdir ${HOME}/kranel
+cd ${HOME}/kranel || exit
+
+#Compiler setup
+
+#Proton Clang 12 by dragun
+git clone https://github.com/kdrag0n/proton-clang.git --depth=1 proton-clang
+
+#AOSP Clang and AOSP GCC for cross compilation
+# git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 aosp-clang
+# git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 aosp-gcc64
+# git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 aosp-gcc32
+
+#AnyKernel setup
+git clone https://github.com/nem0-z/AnyKernel3 -b guacamole --depth=1 anykernel3
+
+#Karamel clone
+git clone git@github.com:nem0-z/android_kernel_oneplus_sm8150.git
+
+cd android_kernel_oneplus_sm8150 || cd .. || exit
+
+echo "Ready to build bish!"
+
+
+
